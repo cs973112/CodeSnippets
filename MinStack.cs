d864d02ba9562260ee4ad6e1d5c4be2f00649ef4
haram <haram@DESKTOP-9Q3DLOR>
2021-08-29 10:18:51 -0400

    Add project files.

---
+++ MinStack.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace InterviewCityNational
+{
+    //public class MinStack<T> : IComparable<T>
+    public class MinStack<T> where T : IComparable
+    {
+        private readonly Stack<T> stack = new Stack<T>();
+        private T min;
+        
+        public MinStack()
+        {
+            stack = new Stack<T>();
+            min = default(T);
+
+        }
+        public void Push(T val)
+        {
+            stack.Push(val);
+            var minvalue = this.GetMin();
+            
+
+            if (val.CompareTo(minvalue) <= 0)
+            {
+                min = val;
+            }
+
+        }
+
+        public void Pop()
+        {
+            var popvalue = stack.Pop();
+            var minvalue = this.GetMin();
+
+            if (popvalue.CompareTo(minvalue) == 0)
+            {
+                var elements = stack.ToList<T>();
+                min = elements.Min();
+            }
+        }
+
+        public T Top() => stack.Peek();
+        public T GetMin() => min;
+
+
+        //    public int CompareTo(T? other)
+        //    {
+        //        throw new NotImplementedException();
+        //    }
+        //}
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InterviewCityNational
+{
+    class Program
+    {
+        /**
+         * 
+         * Author: HarAman Singh Chadha
+         */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            MinStack<int> minStack = new MinStack<int>();
+            minStack.Push(-2);
+            minStack.Push(0);
+            minStack.Push(-3);
+            Console.WriteLine(minStack.GetMin()); // return -3
+            minStack.Pop();
+            minStack.Pop();    // return 0
+            Console.WriteLine(minStack.GetMin()); // return -2
+          
+            Console.WriteLine("() => " + IsParanthesesValid("()"));
+            Console.WriteLine("()[]{} => " + IsParanthesesValid("()[]{}"));
+            Console.WriteLine("(] => " + IsParanthesesValid("(]"));
+            Console.WriteLine("([)] => " + IsParanthesesValid("([)]"));
+            Console.WriteLine("{[]} => " + IsParanthesesValid("{[]}"));
+            
+
+        }
+
+        private static bool IsParanthesesValid(string input)
+        {
+            //check odd length
+            if (string.IsNullOrEmpty(input) || input.Length % 2 != 0) return false;
+            //push first half in stack, pop them and compare
+            // this is the case ({[]})
+            Stack<char> data = new Stack<char>();
+            int halfLength = input.Length / 2;
+            var firstHalfAndSecondHalfMatch = true;
+            var consecutiveMatch = true;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (i < halfLength)
+                {
+                    data.Push(input[i]);
+                }
+                else
+                {
+                    var firstHalfChar = data.Pop();
+                    if ((firstHalfChar == '(' && input[i] == ')') || (firstHalfChar == '{' && input[i] == '}') || (firstHalfChar == '[' && input[i] == ']'))
+                    {
+                        continue;          
+                       
+                    } else
+                    {
+                        firstHalfAndSecondHalfMatch = false;
+                        break;
+                    }
+                                  
+                }
+            }
+            if (firstHalfAndSecondHalfMatch) return true;
+            //check for ()[]{}..
+            data.Clear();
+            for (int i = 0; i < input.Length; i++)
+            {
+                data.Push(input[i]);
+            }
+            for (int i = 0; i < halfLength; i++)
+            {
+                var lastChar = data.Pop();
+                var secondLastChar = data.Pop();
+                if ((secondLastChar == '(' && lastChar == ')') || (secondLastChar == '{' && lastChar == '}') || (secondLastChar == '[' && lastChar == ']'))
+                {
+                    continue;
+
+                }
+                else
+                {
+                    consecutiveMatch = false;
+                    break;
+                }
+            }
+            
+            return consecutiveMatch;
+
+
+        }
+    }
+}
